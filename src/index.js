d60883f826dae7856b9c2c50bc54cb965b95957e
orest <oresttrue@gmail.com>
2020-09-25 16:54:21 +0300

    added component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const el = <div><h1>Hello world!!!</h1>
-<p>Lorem ipsum bla bla</p>
+// React Component
+
+function List() {
+  return (
+    <ul>
+      <li>List item 1</li>
+      <li>List item 1</li>
+    </ul>
+  )
+}
+
+// React Element
+const el =
+  <div>
+    <h1>Hello world!!!</h1>
+    <p>Lorem ipsum bla bla</p>
+    <List/>
   </div>
 
-ReactDom.render(el,document.getElementById("root"))
+ReactDom.render(el, document.getElementById("root"))
