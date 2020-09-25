d0a526d95206b0ab034cf392f8ad28cb070d8887
orest <oresttrue@gmail.com>
2020-09-25 16:43:51 +0300

    style JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const el = React.createElement(
-  'h1',
-  null,
-  "Hello world"
-)
+const el = <h1>Hello world!!!</h1>
 
 ReactDom.render(el,document.getElementById("root"))
