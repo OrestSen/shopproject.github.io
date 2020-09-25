0f3dbdfa0d6f42bd3398f62df7c878ff529a391e
orest <oresttrue@gmail.com>
2020-09-25 17:12:38 +0300

    created component in app.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from './App'
+
+
 
 // React Component
 
 
-const Header = () => {
-  return <h1>Hello world!!!React!!!</h1>
+// const Header = () => {
+//   return <h1>Hello world!!!React!!!</h1>
 
-}
+// }
 
-const List = () => {
-  return (
-    <ul>
-      <li>List item 1</li>
-      <li>List item 2</li>
-      <li>List item 3</li>
-      <li>List item 4</li>
-    </ul>
-  )
-}
+// const List = () => {
+//   return (
+//     <ul>
+//       <li>List item 1</li>
+//       <li>List item 2</li>
+//       <li>List item 3</li>
+//       <li>List item 4</li>
+//     </ul>
+//   )
+// }
 
-// React Element
-const el =
-  <div>
-    <Header/>
-    <p>Lorem ipsum bla bla</p>
-    <List />
-  </div>
+// // React Element
+// const el =
+//   <div>
+//     <Header/>
+//     <p>Lorem ipsum bla bla</p>
+//     <List />
+//   </div>
 
-ReactDom.render(el, document.getElementById("root"))
+ReactDom.render(
+  <App />, document.getElementById("root")
+)
