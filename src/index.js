ff8e29d1ec38e70ca01fcf0e178d258de0612af9
orest <oresttrue@gmail.com>
2020-09-25 17:00:12 +0300

    wich arow style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom'
 
 // React Component
 
-function List() {
+const List = () => {
   return (
     <ul>
       <li>List item 1</li>
-      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+      <li>List item 4</li>
     </ul>
   )
 }
@@ -17,7 +19,7 @@ const el =
   <div>
     <h1>Hello world!!!</h1>
     <p>Lorem ipsum bla bla</p>
-    <List/>
+    <List />
   </div>
 
 ReactDom.render(el, document.getElementById("root"))
