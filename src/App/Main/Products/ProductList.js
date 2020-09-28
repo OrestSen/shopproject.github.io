db7e7f2c0f61a2eb3c393af4b6e2ea91c41bf4be
orest <oresttrue@gmail.com>
2020-09-28 18:03:44 +0300

    created productsData.js end added obj article

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,31 +1,26 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import productsData from './productsData'
 
 const ProductList = () => {
     return (
         <div className="product-list">
             <h1 className="page-title">Product List</h1>
             <div className="row">
+            {
+                productsData.map((product)=>(
                 <div className="col-lg-6">
-                    <ProductListItem 
-                        name="iPhone X"
+                    <ProductListItem
+                        name={"iPhone X"}
+                        description="This is iPhone X"
+                        type="phone"
+                        capacity="256"
+                        price="1500"
                     />
                 </div>
-                <div className="col-lg-6">
-                    <ProductListItem 
-                        name="iPhone XS"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem 
-                        name="iPhone 8 plus"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem 
-                        name="iPhone 8"
-                    />
-                </div>
+                ))
+                    
+                }
             </div>
         </div>
     )
--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -2,17 +2,18 @@ import React from 'react'
 import './ProductListItem.css'
 
 const ProductListItem = (props) => {
+    console.log(props)
     return (
         <div className="product-list-item">
             <div className="product-title">{props.name}</div>
-            <div className="product-description">This is iPhone X</div>
-            <div className="product-features">Type: phone</div>
-            <div className="product-features">Capacity: 64 Gb</div>
-            <div className="product-price">$ 500</div>
+            <div className="product-description">{props.description}</div>
+            <div className="product-features">Type: {props.type}</div>
+            <div className="product-features">Capacity: {props.capacity}</div>
+            <div className="product-price">$ {props.price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
+            <div></div>
         </div>  
     )
 }
-
 
 export default ProductListItem
