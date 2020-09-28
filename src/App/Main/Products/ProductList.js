91c76b8f043f3e5e188e4b1243f88cb3c04c029f
orest <oresttrue@gmail.com>
2020-09-28 17:35:42 +0300

    used props.name with productItem

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -7,7 +7,24 @@ const ProductList = () => {
             <h1 className="page-title">Product List</h1>
             <div className="row">
                 <div className="col-lg-6">
-                    <ProductListItem />
+                    <ProductListItem 
+                        name="iPhone X"
+                    />
+                </div>
+                <div className="col-lg-6">
+                    <ProductListItem 
+                        name="iPhone XS"
+                    />
+                </div>
+                <div className="col-lg-6">
+                    <ProductListItem 
+                        name="iPhone 8 plus"
+                    />
+                </div>
+                <div className="col-lg-6">
+                    <ProductListItem 
+                        name="iPhone 8"
+                    />
                 </div>
             </div>
         </div>
--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import './ProductListItem.css'
 
-const ProductListItem = () => {
+const ProductListItem = (props) => {
     return (
         <div className="product-list-item">
-            <div className="product-title">iPhone X </div>
-            <div className="product-description">This is iPhine X</div>
+            <div className="product-title">{props.name}</div>
+            <div className="product-description">This is iPhone X</div>
             <div className="product-features">Type: phone</div>
             <div className="product-features">Capacity: 64 Gb</div>
             <div className="product-price">$ 500</div>
             <button className="btn-add-to-cart">Add to cart</button>
-        </div>
+        </div>  
     )
 }
+
 
 export default ProductListItem
